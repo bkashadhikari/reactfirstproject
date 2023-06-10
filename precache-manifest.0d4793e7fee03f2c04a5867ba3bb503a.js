14bbb01e1cfc57b0ab8294b3fa360d854445b2b0
bkashadhikari <82298796+bkashadhikari@users.noreply.github.com>
2023-06-10 22:13:20 +0545

    Updates

---
+++ precache-manifest.0d4793e7fee03f2c04a5867ba3bb503a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe54508bb2f44b5666bd",
+    "url": "/reactfirstproject/static/css/main.399b7f5a.chunk.css"
+  },
+  {
+    "revision": "fe54508bb2f44b5666bd",
+    "url": "/reactfirstproject/static/js/main.fe54508b.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/reactfirstproject/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/reactfirstproject/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "8874874b163ad398370f",
+    "url": "/reactfirstproject/static/js/runtime~main.8874874b.js"
+  },
+  {
+    "revision": "2e7b83d68700da302f089f34f1502290",
+    "url": "/reactfirstproject/index.html"
+  }
+];
